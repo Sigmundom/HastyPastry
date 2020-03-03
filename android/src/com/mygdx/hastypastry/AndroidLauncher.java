95cecdce4d09a20ef6812d59742c595557ae0ec5
DESKTOP-306INQD\Sigmund <sigmund.mestad@tekonologiporten.no>
2020-03-03 12:58:31 +0100

    initial commit

---
+++ android/src/com/mygdx/hastypastry/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.hastypastry;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.hastypastry.HastyPastry;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new HastyPastry(), config);
+	}
+}
---
+++ desktop/src/com/mygdx/hastypastry/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.hastypastry.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.hastypastry.HastyPastry;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HastyPastry(), config);
+	}
+}
