d03e6541d58dfffbf506e3f1a38a7f1f0a5d7e29
tare1914 <tomare97@gmail.com>
2020-03-05 11:34:35 +0100

    Added game loop in playState

--- core/src/com/mygdx/hastypastry/models/Waffle.java
+++ core/src/com/mygdx/hastypastry/models/Waffle.java
@@ -7,22 +7,21 @@ import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class Waffle {
-    private World world;
     private Body wBody;
 
     private static final float WAFFLE_RADIUS = 20;
 
-    public void Waffle(float start_X, float start_Y) {
+    public Waffle(World world, float start_X, float start_Y) {
         BodyDef def = new BodyDef();
         def.type = BodyDef.BodyType.DynamicBody;
         def.position.set(start_X, start_Y);
         def.fixedRotation = false;
+        wBody = world.createBody(def);
 
         CircleShape shape = new CircleShape();
         shape.setRadius(WAFFLE_RADIUS);
 
         wBody.createFixture(shape, 1.0f);
-        wBody = world.createBody(def);
 
         shape.dispose();
     }
--- core/src/com/mygdx/hastypastry/states/PlayState.java
+++ core/src/com/mygdx/hastypastry/states/PlayState.java
@@ -1,11 +1,31 @@
 package com.mygdx.hastypastry.states;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
+import com.mygdx.hastypastry.models.Waffle;
 
 public class PlayState extends GameState{
 
-    public PlayState(GameStateManager gsm) {
+    private OrthographicCamera cam;
+    private Box2DDebugRenderer b2dr;
+    private World world;
+    private Waffle waffle;
+
+    public PlayState(GameStateManager gsm, float start_x, float start_y) {
         super(gsm);
+
+        cam = new OrthographicCamera();
+        cam.setToOrtho(false, Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+
+        world = new World(new Vector2(0, -9.81f), false);
+        b2dr = new Box2DDebugRenderer();
+        waffle = new Waffle(world, start_x, start_y);
     }
 
     @Override
@@ -15,16 +35,22 @@ public class PlayState extends GameState{
 
     @Override
     public void update(float dt) {
+        world.step(1/60f, 6,2 );
 
     }
 
     @Override
     public void render(SpriteBatch sb) {
+        update(Gdx.graphics.getDeltaTime());
+        Gdx.gl.glClearColor(0.5f, 0.8f, 1f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        b2dr.render(world, cam.combined);
     }
 
     @Override
     public void dispose() {
-
+        b2dr.dispose();
+        world.dispose();
     }
 }
