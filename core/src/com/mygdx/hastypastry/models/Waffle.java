f0c5b5822e0954569c7adeba104e37222e65b8fa
tare1914 <tomare97@gmail.com>
2020-03-05 12:15:14 +0100

    Added waffle texture and draw in playstate

--- core/src/com/mygdx/hastypastry/models/Waffle.java
+++ core/src/com/mygdx/hastypastry/models/Waffle.java
@@ -1,13 +1,17 @@
 package com.mygdx.hastypastry.models;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class Waffle {
     private Body wBody;
+    private Texture texture;
+    private Vector2 position;
 
     private static final float WAFFLE_RADIUS = 20;
 
@@ -15,11 +19,14 @@ public class Waffle {
         BodyDef def = new BodyDef();
         def.type = BodyDef.BodyType.DynamicBody;
         def.position.set(start_X, start_Y);
+        position = new Vector2(start_X, start_Y);
         def.fixedRotation = false;
         wBody = world.createBody(def);
 
         CircleShape shape = new CircleShape();
         shape.setRadius(WAFFLE_RADIUS);
+
+        texture = new Texture("Waffle.png");
 
         wBody.createFixture(shape, 1.0f);
 
@@ -30,8 +37,12 @@ public class Waffle {
         world.step(1/60f, 6, 2);
     }*/
 
-    public Body getWaffle() {
-        return wBody;
+    public Texture getWaffle() {
+        return texture;
+    }
+
+    public Vector2 getPosition() {
+        return position;
     }
 
 }
