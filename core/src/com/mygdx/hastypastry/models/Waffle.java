a3248f845538961be8675f25562f1bc2839ef961
tare1914 <tomare97@gmail.com>
2020-03-09 11:43:09 +0100

    Added sprite to Waffle, not fin

--- core/src/com/mygdx/hastypastry/models/Waffle.java
+++ core/src/com/mygdx/hastypastry/models/Waffle.java
@@ -1,6 +1,8 @@
 package com.mygdx.hastypastry.models;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
@@ -12,8 +14,10 @@ public class Waffle {
     private Body wBody;
     private Texture texture;
     private Vector2 position;
+    private Sprite sprite;
+    private float rotation;
 
-    private static final float WAFFLE_RADIUS = 20;
+    private static final float WAFFLE_RADIUS = 7;
 
     public Waffle(World world, float start_X, float start_Y) {
         BodyDef def = new BodyDef();
@@ -26,23 +30,32 @@ public class Waffle {
         CircleShape shape = new CircleShape();
         shape.setRadius(WAFFLE_RADIUS);
 
-        texture = new Texture("Waffle.png");
+        sprite = new Sprite(new Texture("Waffle.png"));
 
         wBody.createFixture(shape, 1.0f);
-
         shape.dispose();
     }
 
-    /*public void update() {
-        world.step(1/60f, 6, 2);
-    }*/
+    public void update() {
+        rotation = (float)Math.toDegrees(wBody.getAngle());
+        //sprite.setPosition(position.x, position.y);
+        sprite.setRotation(rotation);
+    }
+
+    public void render(SpriteBatch sb) {
+        update();
+        sprite.draw(sb);
+    }
 
     public Texture getWaffle() {
-        return texture;
+        return sprite.getTexture();
     }
 
     public Vector2 getPosition() {
         return position;
     }
 
+    public void dispose() {
+
+    }
 }
