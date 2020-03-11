4c8337f5ddf45d0d67ba238628b1ba02c432f428
superfreek <philipae@stud.ntnu.no>
2020-03-11 13:25:05 +0100

    Added Main menu botton

--- core/src/com/mygdx/hastypastry/views/MainMenuView.java
+++ core/src/com/mygdx/hastypastry/views/MainMenuView.java
@@ -15,6 +15,7 @@ public class MainMenuView extends BaseView {
 
     @Override
     public void buildStage() {
+
         SinglePlayerBtn = new MenuButton("Single Player", ScreenEnum.PLAY);
         SinglePlayerBtn.setPosition(Config.WIDTH/2 - SinglePlayerBtn.getWidth() - 10,Config.HEIGHT/2);
         MultiPlayerBtn = new MenuButton("Multiplayer", ScreenEnum.LOBBY);
