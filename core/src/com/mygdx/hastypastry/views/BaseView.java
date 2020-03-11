f6c3d1acfc846be3fc28917beef5fe01b6b22703
DESKTOP-306INQD\Sigmund <sigmund.mestad@tekonologiporten.no>
2020-03-11 14:14:13 +0100

    Added camera, not finished

--- core/src/com/mygdx/hastypastry/views/BaseView.java
+++ core/src/com/mygdx/hastypastry/views/BaseView.java
@@ -31,11 +31,11 @@ public abstract class BaseView implements Screen {
     @Override
     public void render(float delta) {
         // Clear screen
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         spriteBatch.begin();
-        spriteBatch.draw(background, 0,0, Config.WIDTH, Config.HEIGHT);
         // custom drawing
+        spriteBatch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         draw(spriteBatch, delta);
         spriteBatch.end();
 
--- core/src/com/mygdx/hastypastry/views/PlayView.java
+++ core/src/com/mygdx/hastypastry/views/PlayView.java
@@ -1,5 +1,7 @@
 package com.mygdx.hastypastry.views;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
@@ -12,8 +14,14 @@ import com.mygdx.hastypastry.ui.MenuButton;
 
 import java.awt.Menu;
 
+import static com.mygdx.hastypastry.Config.POSITION_ITERATIONS;
+import static com.mygdx.hastypastry.Config.SCALE;
+import static com.mygdx.hastypastry.Config.TIME_STEP;
+import static com.mygdx.hastypastry.Config.VELOCITY_ITERATIONS;
+
 public class PlayView extends BaseView {
     private Box2DDebugRenderer b2dr;
+    private OrthographicCamera orthographicCamera;
     private World world;
     private Waffle waffle;
     private RoundObstacle roundObstacle;
@@ -22,16 +30,23 @@ public class PlayView extends BaseView {
     public PlayView() {
         super();
         world = new World(new Vector2(0, -9.81f), false);
+        orthographicCamera = new OrthographicCamera();
+        orthographicCamera.setToOrtho(false, Gdx.graphics.getWidth()/SCALE, Gdx.graphics.getHeight()/SCALE);
         b2dr = new Box2DDebugRenderer();
         waffle = new Waffle(world, Config.WIDTH/2, Config.HEIGHT - 50);
         roundObstacle = new RoundObstacle(world, new Vector2(50, 50), false);
     }
 
     @Override
-    public void draw(SpriteBatch spriteBatch, float delta) {
-        spriteBatch.draw(roundObstacle.getTexture(), roundObstacle.getPos().x, roundObstacle.getPos().y, 45, 44);
-        waffle.draw(spriteBatch);
+    public void draw(SpriteBatch batch, float delta) {
+        world.step(TIME_STEP, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+        batch.setProjectionMatrix(orthographicCamera.combined);
+        batch.draw(roundObstacle.getTexture(), roundObstacle.getPos().x, roundObstacle.getPos().y, 45, 44);
+        waffle.draw(batch);
     }
+
+
+
     @Override
     public void buildStage() {
         MenuBtn = new MenuButton("Main Menu", ScreenEnum.MAIN_MENU);
