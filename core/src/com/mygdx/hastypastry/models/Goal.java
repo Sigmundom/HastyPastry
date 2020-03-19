75daddf79d0aca90bfda18adc1256cf8e5c83c5c
Kristoffer Lehre Fagerheim <kristoffer.lf@gmail.com>
2020-03-19 11:29:26 +0100

    Added image for goal

---
+++ core/src/com/mygdx/hastypastry/models/Goal.java
@@ -0,0 +1,17 @@
+package com.mygdx.hastypastry.models;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class Goal {
+
+    private Body body;
+    private Sprite sprite;
+
+    public Goal(World world, float posX, float posY){
+
+        Sprite sprite = new Sprite();
+
+    }
+}
